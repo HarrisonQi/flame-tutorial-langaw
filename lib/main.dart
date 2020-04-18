b3c9b70ae9629ba7a16689cfdc41102180c57eaa
阿航 <harrisonqi@foxmail.com>
2020-04-18 10:41:27 +0800

    :tada: 第一步:创建一个Flame/Flutter游戏

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flame/util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+}
